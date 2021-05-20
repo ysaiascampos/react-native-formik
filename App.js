3e0acfd8d8bf918dc42fab4567319db1c6b689a3
ysaiascampos <ysaiascampos@gmail.com>
2021-05-19 21:44:01 -0400

    validate forma manual

--- App.js
+++ App.js
@@ -3,20 +3,33 @@ import React from 'react';
 import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 import { useFormik } from 'formik';
 
+const validate = values => {
+  const errors = {};
+  if (!values.email) {
+    errors.email = 'Requerido'
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+    errors.email = 'Correo invalido';
+  }
+  return errors
+}
 export default function App() {
   const formik = useFormik({
     initialValues: {
-      email: 'lala@lala.com'
+      email: ''
     },
+    validate,
     onSubmit: x => console.warn(x)
   })
   return (
     <View style={styles.container}>
       <Text>Correo Electronico</Text>
       <TextInput 
+        onBlur={formik.handleBlur('email')}
+        style={styles.input}
         onChangeText={formik.handleChange('email')}
         value={formik.values.email}
       />
+      {formik.errors.email && formik.touched.email ? <Text>{formik.errors.email}</Text>:null}
       <Button 
         title='Enviar'
         onPress={formik.handleSubmit}
@@ -33,4 +46,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input: {
+    height: 30,
+    width: 150,
+    fontSize: 15,
+    backgroundColor: '#eee',
+
+  }
 });
