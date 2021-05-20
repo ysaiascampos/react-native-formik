3692efc5261170c4031da37ab3c8db74ce5c8430
ysaiascampos <ysaiascampos@gmail.com>
2021-05-19 21:27:09 -0400

    inicio

--- App.js
+++ App.js
@@ -1,11 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { useFormik } from 'formik';
 
 export default function App() {
+  const formik = useFormik({
+    initialValues: {
+      email: 'lala@lala.com'
+    },
+    onSubmit: x => console.warn(x)
+  })
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Correo Electronico</Text>
+      <TextInput 
+        onChangeText={formik.handleChange('email')}
+        value={formik.values.email}
+      />
+      <Button 
+        title='Enviar'
+        onPress={formik.handleSubmit}
+      />
       <StatusBar style="auto" />
     </View>
   );
