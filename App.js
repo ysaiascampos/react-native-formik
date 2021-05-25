755ec29bb710913875d83972b0b5f55a4fd4e32a
ysaiascampos <ysaiascampos@gmail.com>
2021-05-24 20:39:26 -0400

    [field, meta] = useField(fieldName)

--- App.js
+++ App.js
@@ -1,18 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
-import { Formik, useFormikContext } from 'formik';
+import { Formik, useFormikContext, useField } from 'formik';
 import * as Yup from 'yup';
 
+const MyInput = ({ fieldName, ...props }) => {
+  const [field, meta] = useField(fieldName);
+  return (
+    <>
+      <TextInput 
+        style={styles.input}
+        onChangeText={field.onChange(fieldName)}
+        onBlur={field.onBlur(fieldName)}
+        value={field.values}
+        {...props}
+      />
+      {meta.error && meta.touched && (
+        <Text style={{ color: 'red' }}>{meta.error}</Text>
+      )}
+    </>
+  )
+}
 const EmailForm = () => {
-  const { handleChange, submitForm, values } = useFormikContext();
+  const { submitForm } = useFormikContext();
   return (
     <>
       <Text>Correo Electronico</Text>
-      <TextInput 
-        style={styles.input}
-        onChangeText={handleChange('email')}
-        value={values.email}
+      <MyInput
+        fieldName="email" 
+      />
+      <Text>Nombre</Text>
+      <MyInput
+        fieldName="name" 
       />
       <Button
         title="Enviar"
@@ -27,12 +46,16 @@ export default function App() {
     <View style={styles.container}
     >
       <Formik 
+        onSubmit={x => console.log(x)}
         validationSchema={Yup.object({
           email: Yup.string()
           .email('Correo invalido')
-          .required('Correo Requerido')
+          .required('Correo Requerido'),
+          name: Yup.string()
+          .min(5)
+          .required('Nombre Requerido')
         })}
-        initialValues={{email: ''}}
+        initialValues={{email: '',name: ''}}
       >
         <EmailForm />
       </Formik>
